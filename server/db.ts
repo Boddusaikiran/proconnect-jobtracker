8bdc3ad161992858a90998d0fc3126f382d20686
boddu saikiran <boddu.saikiran03@gmail.com>
2025-11-21 23:11:02 +0530

    Describe your changes

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-
+// hvhgfgjgv
 const connectionString = process.env.DATABASE_URL!;
 const pool = new Pool({ connectionString });
 export const db = drizzle(pool);
